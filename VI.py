09ecc3ee25ffd9d71ba533f92ec139d559ff07ab
Altay Dikme <altay@kth.se>
2020-12-23 19:08:55 +0100

    Needs further testing. However should work.

--- VI.py
+++ VI.py
@@ -2,12 +2,13 @@ import numpy as np
 from scipy.special import digamma
 from sklearn.preprocessing import normalize
 
+
 def VI(k, N, alpha, beta):
     ''' Input:
     K = number of topics
     N = number of words
     alpha = vector (size K) of Dirichlet concentration parametres
-    beta = matrix (size k x V) of word probabilities
+    beta = matrix (size k x N) of word probabilities
     '''
 
     #phi_0 = n x k matrix
@@ -20,12 +21,12 @@ def VI(k, N, alpha, beta):
     while (True):
         for n in range(N):
             for i in range(k):
-                phi_t1[n,i] = beta[i,n] * np.exp(digamma(gamma_0[i]))   #Unsure if correct dimensions
+                phi_t1[n,i] = beta[i,n] * np.exp(digamma(gamma_0[i]))  #Something strange is going on with the digamma function
             phi_t1 = normalize(phi_t1, axis = 1, norm = 'l1')
-        gamma_t1 = alpha + np.ndarray.sum(phi_t1,axis = 1)              #Does not work properly
+        gamma_t1 = alpha + np.sum(phi_t1,axis = 0)
 
-        #print(np.abs(gamma_0 -gamma_t1))#<0.01:                        #Need to implement proper convergence criterion
-            #   break                                                   #What is a good criterion?
+        if np.abs(gamma_0-gamma_t1).any()<0.01:     #Need to implement proper convergence criterion
+            break                                   #What is a good criterion?
         phi_t1 = phi_0
         gamma_t1 = gamma_0
 
