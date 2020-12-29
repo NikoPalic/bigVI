33e7fb73a3133631323beef576f24c86e4f37f64
Semih Kurt <68644299+Semih-Kurt@users.noreply.github.com>
2020-12-29 17:11:26 +0100

    Add files via upload

---
+++ document_modeling.py
@@ -0,0 +1,51 @@
+import numpy as np
+
+
+def perplexity_of_corpus(K, corpus, alpha, beta):
+    ''' Input:
+    K = number of topics
+    corpus = collection of M documents
+                (each document is a sequence of N_i integer numbers (numbers correspond to indices at vocabulary))
+    alpha = vector (size K) - optimized alpha parameters
+    beta = matrix (size (K x V)) -optimized beta parameters
+    '''
+
+    M = len(corpus)
+    N = [len(document) for document in corpus]
+
+    log_likelihood_corpus=0
+    for doc_num in range(M):
+        log_likelihood_corpus += np.log(likelihood_of_document(K, corpus[doc_num], alpha, beta))
+
+    word_count_corpus=0
+    for doc_num in range(M):
+        word_count_corpus += N[doc_num]
+
+    perplexity = np.exp((-1 * log_likelihood_corpus) / word_count_corpus)
+    return perplexity
+
+
+def likelihood_of_document(K, document, alpha, beta):
+    ''' Input:
+    K = number of topics
+    document = sequence of N_i integer numbers (numbers correspond to indices at vocabulary))
+    alpha = vector (size K) - optimized alpha parameters
+    beta = matrix (size (K x V)) -optimized beta parameters
+    '''
+
+    N_i = len(document)
+    S = 1000  # number of samples used in Importance Sampling method
+    theta_array = np.random.dirichlet(alpha, S)
+    prob_of_doc=0
+    for s in range(S):
+        theta_s=theta_array[s]
+        prob_doc_given_theta_s = 1
+        for w in range(N_i):  # w corresponds word index
+            prob_of_w = 0
+            for z in range(K):  # z corresponds topic index
+                prob_of_w += theta_s[z]*beta[z][w]
+            prob_doc_given_theta_s = prob_doc_given_theta_s * prob_of_w
+        prob_of_doc += prob_doc_given_theta_s
+    prob_of_doc = prob_of_doc / S
+    return prob_of_doc
+
