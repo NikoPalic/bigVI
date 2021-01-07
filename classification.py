61e9d15c9653e4d38ec7a962777b52b284676126
Shervin <60320885+shervin-gohari@users.noreply.github.com>
2021-01-07 18:42:55 +0100

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,4 +1,5 @@
-from reuters_parser import parse_reuters
+from reuters_parser import reuters_parse_multiple
+import numpy as np
 
 # List of filenames
 filenames_full = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-001.sgm",
@@ -13,16 +14,11 @@ filenames_full = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-00
              "data/reuters21578/reut2-018.sgm","data/reuters21578/reut2-019.sgm",
              "data/reuters21578/reut2-020.sgm", "data/reuters21578/reut2-021.sgm"
              ]
+
 filenames_testing = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-001.sgm",
              "data/reuters21578/reut2-002.sgm", "data/reuters21578/reut2-003.sgm",
              "data/reuters21578/reut2-004.sgm", "data/reuters21578/reut2-005.sgm",
              "data/reuters21578/reut2-006.sgm", "data/reuters21578/reut2-007.sgm",
              ]
-full_classes = []
-full_corpus = []
 
-for file in filenames_testing:
-    classes, corpus = parse_reuters(file,"grain")
-    full_classes.append(classes)
-    full_corpus.append(corpus)
-
+classes, corpus = reuters_parse_multiple(filenames_testing,"grain")
