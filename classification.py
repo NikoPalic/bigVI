b50da140f7322894e7c8eff22e6656fdd895f040
Shervin <60320885+shervin-gohari@users.noreply.github.com>
2021-01-07 16:11:12 +0100

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,7 +1,7 @@
 from reuters_parser import parse_reuters
 
 # List of filenames
-filenames = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-001.sgm",
+filenames_full = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-001.sgm",
              "data/reuters21578/reut2-002.sgm", "data/reuters21578/reut2-003.sgm",
              "data/reuters21578/reut2-004.sgm", "data/reuters21578/reut2-005.sgm",
              "data/reuters21578/reut2-006.sgm", "data/reuters21578/reut2-007.sgm",
@@ -13,11 +13,16 @@ filenames = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-001.sgm
              "data/reuters21578/reut2-018.sgm","data/reuters21578/reut2-019.sgm",
              "data/reuters21578/reut2-020.sgm", "data/reuters21578/reut2-021.sgm"
              ]
+filenames_testing = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-001.sgm",
+             "data/reuters21578/reut2-002.sgm", "data/reuters21578/reut2-003.sgm",
+             "data/reuters21578/reut2-004.sgm", "data/reuters21578/reut2-005.sgm",
+             "data/reuters21578/reut2-006.sgm", "data/reuters21578/reut2-007.sgm",
+             ]
 full_classes = []
 full_corpus = []
 
-for file in filenames:
+for file in filenames_testing:
     classes, corpus = parse_reuters(file,"grain")
     full_classes.append(classes)
     full_corpus.append(corpus)
-    
+
