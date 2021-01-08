a5177d2a21652590b4bf5f4e5eb394c9e612a8d3
Semih Kurt <68644299+Semih-Kurt@users.noreply.github.com>
2021-01-08 20:04:05 +0100

    Update document_modeling.py

--- document_modeling.py
+++ document_modeling.py
@@ -1,4 +1,6 @@
 import numpy as np
+import sys
+import myVI
 
 
 def perplexity_of_corpus(K, corpus, alpha, beta):
@@ -15,7 +17,7 @@ def perplexity_of_corpus(K, corpus, alpha, beta):
 
     log_likelihood_corpus=0
     for doc_num in range(M):
-        log_likelihood_corpus += np.log(likelihood_of_document(K, corpus[doc_num], alpha, beta))
+        log_likelihood_corpus += log_likelihood_of_document(K, corpus[doc_num], alpha, beta)
 
     word_count_corpus=0
     for doc_num in range(M):
@@ -25,7 +27,7 @@ def perplexity_of_corpus(K, corpus, alpha, beta):
     return perplexity
 
 
-def likelihood_of_document(K, document, alpha, beta):
+def log_likelihood_of_document(K, document, alpha, beta):
     ''' Input:
     K = number of topics
     document = sequence of N_i integer numbers (numbers correspond to indices at vocabulary))
@@ -39,12 +41,12 @@ def likelihood_of_document(K, document, alpha, beta):
     prob_of_doc=0
     for s in range(S):
         theta_s=theta_array[s]
-        prob_doc_given_theta_s = 1
+        prob_doc_given_theta_s = 0
         for w in range(N_i):  # w corresponds word index
             prob_of_w = 0
             for z in range(K):  # z corresponds topic index
-                prob_of_w += theta_s[z]*beta[z][w]
-            prob_doc_given_theta_s = prob_doc_given_theta_s * prob_of_w
+                prob_of_w += theta_s[z]*beta[z][document[w]]
+            prob_doc_given_theta_s += np.log(prob_of_w + sys.float_info.min)
         prob_of_doc += prob_doc_given_theta_s
     prob_of_doc = prob_of_doc / S
     return prob_of_doc
