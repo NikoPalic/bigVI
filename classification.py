3f2a402fc5ca1f672df062d26316bbaf9acf8fb5
Shervin <60320885+shervin-gohari@users.noreply.github.com>
2021-01-11 20:32:45 +0100

    Update classification.py

--- classification.py
+++ classification.py
@@ -1,4 +1,10 @@
 from reuters_parser import reuters_parse_multiple
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.feature_extraction.text import CountVectorizer
+
+
 # List of filenames
 filenames_full = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-001.sgm",
              "data/reuters21578/reut2-002.sgm", "data/reuters21578/reut2-003.sgm",
@@ -17,6 +23,24 @@ filenames_testing = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2
              "data/reuters21578/reut2-002.sgm", "data/reuters21578/reut2-003.sgm",
              "data/reuters21578/reut2-004.sgm", "data/reuters21578/reut2-005.sgm",
              "data/reuters21578/reut2-006.sgm", "data/reuters21578/reut2-007.sgm",
+             "data/reuters21578/reut2-008.sgm","data/reuters21578/reut2-009.sgm",
+             "data/reuters21578/reut2-010.sgm", "data/reuters21578/reut2-011.sgm",
+             "data/reuters21578/reut2-012.sgm", "data/reuters21578/reut2-013.sgm",
+             "data/reuters21578/reut2-020.sgm", "data/reuters21578/reut2-021.sgm"
              ]
 
 classes, corpus = reuters_parse_multiple(filenames_testing,"grain")
+
+
+
+vectorizer = CountVectorizer(stop_words="english")
+
+X = vectorizer.fit_transform(corpus)
+
+X_train, X_test,y_train, y_test = train_test_split(X, classes, train_size = 0.2, random_state = 42)
+
+
+clf = SVC()
+clf.fit(X_train,y_train)
+preds = clf.predict(X_test)
+score = accuracy_score(y_test, preds)
