ea51d2a28bec34550e7466d7b85cd81f8137813a
Shervin <60320885+shervin-gohari@users.noreply.github.com>
2021-01-14 17:52:56 +0100

    Update classification_LDA.py

--- classification_LDA.py
+++ classification_LDA.py
@@ -34,7 +34,7 @@ filenames_short = ["data/reuters21578/reut2-000.sgm", "data/reuters21578/reut2-0
              ]
 ## Loading the dataset 
 
-dl = ReutersLoader(filenames_short,"earn")
+dl = ReutersLoader(filenames_testing,"earn")
 L = dl.preprocess_dataset() #list of preprocessed documents
 vocabulary = dl.get_vocabulary(L)
 
@@ -45,4 +45,19 @@ K = 2 #assume K number of topics
 alpha, beta, gamma = get_guesses(K, V, M)
 corpus = dl.get_vocab_doc_representation(L)
 alpha, beta, Gamma, Phi = myVI.full_VI(K,corpus, V, alpha, beta, gamma)
+truth = dl.classes
+# Classification
+clf = SVC()
+sizes = [0.01, 0.05, 0.1, 0.2]
+scores = []
+for i in sizes:
+    X_train, X_test,y_train, y_test = train_test_split(Gamma, truth, train_size = i, random_state = 42)
+    clf.fit(X_train,y_train)
+    preds = clf.predict(X_test)
+    score = accuracy_score(y_test, preds)
+    scores.append(score)
+    
+
+# Plotting
+plt.scatter(sizes,scores)
 
